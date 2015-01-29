e6bbcc21f25d840043e3d46fb84881d2e0a26459
Harold Cooper <harold@heatseeknyc.com>
2015-01-29 09:56:49 -0500

    /hubs endpoint for hub metadata (currently just ssh port)

--- app.py
+++ app.py
@@ -22,16 +22,27 @@ def index():
 
 @app.route('/readings', methods=('GET', 'POST'))
 def readings():
-    if flask.request.method == 'GET':
-        cursor = db.cursor()
-        cursor.execute('select * from readings order by time desc limit 100')
-        return flask.render_template('readings.html', readings=cursor.fetchall())
-    elif flask.request.method == 'POST':
+    if flask.request.method == 'POST':
         d = flask.request.form.copy()
         d['time'] = datetime.fromtimestamp(int(d['time']))
         db.cursor().execute('insert into readings values'
                             ' (%(hub)s, %(time)s, %(cell)s, %(temp)s)', d)
-        return ''
+        return 'ok'
+    elif flask.request.method == 'GET':
+        cursor = db.cursor()
+        cursor.execute('select * from readings order by time desc limit 100')
+        return flask.render_template('readings.html', readings=cursor.fetchall())
+
+@app.route('/hubs', methods=('GET', 'POST'))
+def hubs():
+    if flask.request.method == 'POST':
+        db.cursor().execute('insert into hubs values'
+                            ' (%(hub)s, %(port)s)', flask.request.form)
+        return 'ok'
+    elif flask.request.method == 'GET':
+        cursor = db.cursor()
+        cursor.execute('select * from hubs order by time desc limit 100')
+        return flask.render_template('hubs.html', hubs=cursor.fetchall())
 
 if __name__ == '__main__':
     app.run(debug=True)
