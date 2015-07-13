857da183f3ad9f62b2f10669532704d0f005eedd
Harold Cooper <hrldcpr@gmail.com>
2015-07-13 10:34:46 -0400

    use xbee_id as user-facing hub id. splits hubs table into latest `hubs` and historical `hubs_log`

--- app/app.py
+++ app/app.py
@@ -43,40 +43,63 @@ class Hubs(flask.views.MethodView):
     @staticmethod
     def get():
         cursor = db.cursor()
-        cursor.execute('select distinct on (hub_id) hub_id, time, port from hubs'
-                       ' order by hub_id, time desc')
-        hubs = sorted(cursor.fetchall(), key=operator.itemgetter('time'), reverse=True)
-        return flask.render_template('hubs.html', hubs=hubs)
+        cursor.execute('select id, xbee_id, port, time from hubs'
+                       ' order by time desc')
+        return flask.render_template('hubs.html', hubs=cursor.fetchall())
 
     @staticmethod
     def post():
-        db.cursor().execute('insert into hubs (hub_id, port)'
-                            ' values (%(hub)s, %(port)s)', flask.request.form)
+        with db:
+            db.cursor().execute('insert into hubs_log (hub_id, xbee_id, port)'
+                                ' values (%(hub)s, %(xbee)s, %(port)s)', flask.request.form)
+
+        # psotgres can't upsert, so we try to insert, and if not then update:
+        try:
+            with db:
+                db.cursor().execute('insert into hubs (id, xbee_id, port)'
+                                    ' values (%(hub)s, %(xbee)s, %(port)s)', flask.request.form)
+        except psycopg2.IntegrityError:
+            updates = {'time': 'now()'}
+            if flask.request.form.get('xbee'): updates['xbee_id'] = '%(xbee)s'
+            if flask.request.form.get('port'): updates['port'] = '%(port)s'
+            updates = ', '.join('{}={}'.format(k, v) for k, v in updates.items())
+            db.cursor().execute('update hubs set {} where id=%(hub)s'.format(updates),
+                                flask.request.form)
+
         return 'ok'
 
 
-@route('/hubs/<hub_id>', 'hub')
+@route('/hubs/<xbee_id>', 'hub')
 class Hub(flask.views.MethodView):
     @staticmethod
-    def get(hub_id):
+    def get(xbee_id):
+        hub_id = get_hub_id(xbee_id)
+        if not hub_id: flask.abort(404)
+
         cursor = db.cursor()
-        cursor.execute('select time, port from hubs'
+
+        cursor.execute('select time, port from hubs_log'
                        ' where hub_id=%s order by time desc limit 10', (hub_id,))
         logs = cursor.fetchall()
+
         cursor.execute('select cell_id, max(time) as time from readings'
                        ' where hub_id=%s group by cell_id order by time desc', (hub_id,))
         cells = cursor.fetchall()
+
         cursor.execute('select hub_time, cell_id, temperature, relay, relayed_time from readings'
                        ' where hub_id=%s order by hub_time desc limit 1000', (hub_id,))
         readings = cursor.fetchall()
+
         return flask.render_template('hub.html', logs=logs, cells=cells, readings=readings)
 
     @staticmethod
-    def patch(hub_id):
+    def patch(xbee_id):
+        hub_id = get_hub_id(xbee_id)
+        if not hub_id: flask.abort(404)
+
         # TODO actually look at the data, which should be something like hourly=true...
         cursor = db.cursor()
-        cursor.execute('select port from hubs where hub_id=%s'
-                       ' order by time desc limit 1', (hub_id,))
+        cursor.execute('select port from hubs where id=%s and port is not null', (hub_id,))
         row = cursor.fetchone()
         if not row: return 'no ssh port for hub', 404
 
@@ -124,6 +147,13 @@ class Readings(flask.views.MethodView):
         cursor.execute('insert into readings (hub_id, hub_time, cell_id, temperature, relay)'
                        ' values (%(hub)s, %(time)s, %(cell)s, %(temp)s, %(relay)s)', d)
         return 'ok'
+
+
+def get_hub_id(xbee_id):
+    cursor = db.cursor()
+    cursor.execute('select id from hubs where xbee_id=%s', (xbee_id,))
+    row = cursor.fetchone()
+    if row: return row['id']
 
 
 if __name__ == '__main__':
--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 create table readings (
     id serial primary key,
-    hub_id text,
+    hub_id text not null,
     hub_time timestamp not null,
     cell_id text not null
     temperature real not null,
@@ -8,20 +8,22 @@ create table readings (
     time timestamp not null default now(),
     relayed_time timestamp
 );
-create index on readings (hub_id, cell_id, hub_time desc);
+create index on readings (hub_id, hub_time desc);
+create index on readings (cell_id, hub_time desc);
 
 create table hubs (
     id text primary key,
+    xbee_id text,
     port integer,
-    created timestamp not null default now(),
-    updated timestamp not null default now()
+    time timestamp not null default now()
 );
+create index on hubs (xbee_id);
 
 create table hubs_log (
     id serial primary key,
-    hub_id text,
+    hub_id text not null,
+    xbee_id text,
     port integer,
-    created timestamp not null default now(),
-    updated timestamp not null default now()
+    time timestamp not null default now()
 );
-create index on hubs (hub_id, time desc);
+create index on hubs_log (hub_id, time desc);
