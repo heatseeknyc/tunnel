e56dd6e95a476a1fcc75d97197647cedf2fa3367
Harold Cooper <harold@heatseeknyc.com>
2015-08-25 17:42:01 -0400

    use hub's xbee id everywhere, and simplify db schema. still store π id, but only as metadata.

--- app/app.py
+++ app/app.py
@@ -1,12 +1,14 @@
 from datetime import datetime
-import logging; logging.basicConfig(level=logging.INFO)
-import operator
+import logging
 import subprocess
 
 import flask
 import flask.views
 import psycopg2
 import psycopg2.extras
+
+
+logging.basicConfig(level=logging.INFO)
 
 
 app = flask.Flask(__name__)
@@ -43,63 +45,39 @@ class Hubs(flask.views.MethodView):
     @staticmethod
     def get():
         cursor = db.cursor()
-        cursor.execute('select id, xbee_id, port, time from hubs'
-                       ' order by time desc')
+        cursor.execute('select hub_id, max(time) as time from hubs'
+                       ' group by hub_id order by time desc')
         return flask.render_template('hubs.html', hubs=cursor.fetchall())
 
     @staticmethod
     def post():
-        with db:
-            db.cursor().execute('insert into hubs_log (hub_id, xbee_id, port)'
-                                ' values (%(hub)s, %(xbee)s, %(port)s)', flask.request.form)
-
-        # psotgres can't upsert, so we try to insert, and if not then update:
-        try:
-            with db:
-                db.cursor().execute('insert into hubs (id, xbee_id, port)'
-                                    ' values (%(hub)s, %(xbee)s, %(port)s)', flask.request.form)
-        except psycopg2.IntegrityError:
-            updates = {'time': 'now()'}
-            if flask.request.form.get('xbee'): updates['xbee_id'] = '%(xbee)s'
-            if flask.request.form.get('port'): updates['port'] = '%(port)s'
-            updates = ', '.join('{}={}'.format(k, v) for k, v in updates.items())
-            db.cursor().execute('update hubs set {} where id=%(hub)s'.format(updates),
-                                flask.request.form)
-
+        db.cursor().execute('insert into hubs (hub_id, pi_id, port)'
+                            ' values (%(hub)s, %(pi)s, %(port)s)', flask.request.form)
         return 'ok'
 
 
-@route('/hubs/<xbee_id>', 'hub')
+@route('/hubs/<id>', 'hub')
 class Hub(flask.views.MethodView):
     @staticmethod
-    def get(xbee_id):
-        hub_id = get_hub_id(xbee_id)
-        if not hub_id: flask.abort(404)
-
+    def get(id):
         cursor = db.cursor()
-
-        cursor.execute('select time, port from hubs_log'
-                       ' where hub_id=%s order by time desc limit 10', (hub_id,))
+        cursor.execute('select pi_id, port, time from hubs'
+                       ' where hub_id=%s order by time desc limit 10', (id,))
         logs = cursor.fetchall()
-
-        cursor.execute('select cell_id, max(time) as time from readings'
-                       ' where hub_id=%s group by cell_id order by time desc', (hub_id,))
+        cursor.execute('select cell_id, max(time) as time from temperatures'
+                       ' where hub_id=%s group by cell_id order by time desc', (id,))
         cells = cursor.fetchall()
-
-        cursor.execute('select hub_time, cell_id, temperature, relay, relayed_time from readings'
-                       ' where hub_id=%s order by hub_time desc limit 1000', (hub_id,))
-        readings = cursor.fetchall()
-
-        return flask.render_template('hub.html', logs=logs, cells=cells, readings=readings)
+        cursor.execute('select cell_id, temperature, relay, hub_time, relayed_time from temperatures'
+                       ' where hub_id=%s order by hub_time desc limit 100', (id,))
+        temperatures = cursor.fetchall()
+        return flask.render_template('hub.html', logs=logs, cells=cells, temperatures=temperatures)
 
     @staticmethod
-    def patch(xbee_id):
-        hub_id = get_hub_id(xbee_id)
-        if not hub_id: flask.abort(404)
-
+    def patch(id):
         # TODO actually look at the data, which should be something like hourly=true...
         cursor = db.cursor()
-        cursor.execute('select port from hubs where id=%s and port is not null', (hub_id,))
+        cursor.execute('select port from hubs where hub_id=%s and port is not null'
+                       ' order by time desc limit 1', (id,))
         row = cursor.fetchone()
         if not row: return 'no ssh port for hub', 404
 
@@ -108,28 +86,28 @@ class Hub(flask.views.MethodView):
         return 'ok'
 
 
-@route('/cells/<cell_id>', 'cell')
+@route('/cells/<id>', 'cell')
 class Cell(flask.views.MethodView):
     @staticmethod
-    def get(cell_id):
+    def get(id):
         cursor = db.cursor()
-        cursor.execute('select hub_id, max(time) as time from readings'
-                       ' where cell_id=%s group by hub_id order by time desc', (cell_id,))
+        cursor.execute('select hub_id, max(time) as time from temperatures'
+                       ' where cell_id=%s group by hub_id order by time desc', (id,))
         hubs = cursor.fetchall()
-        cursor.execute('select hub_time, hub_id, temperature, relay, relayed_time from readings'
-                       ' where cell_id=%s order by hub_time desc limit 1000', (cell_id,))
-        readings = cursor.fetchall()
-        return flask.render_template('cell.html', hubs=hubs, readings=readings)
+        cursor.execute('select hub_id, temperature, relay, hub_time, relayed_time from temperatures'
+                       ' where cell_id=%s order by hub_time desc limit 100', (id,))
+        temperatures = cursor.fetchall()
+        return flask.render_template('cell.html', hubs=hubs, temperatures=temperatures)
 
 
-@route('/readings', 'readings')
-class Readings(flask.views.MethodView):
+@route('/temperatures', 'temperatures')
+class Temperatures(flask.views.MethodView):
     @staticmethod
     def get():
         cursor = db.cursor()
-        cursor.execute('select hub_time, hub_id, cell_id, temperature, relay, relayed_time'
-                       ' from readings order by hub_time desc limit 1000')
-        return flask.render_template('readings.html', readings=cursor.fetchall())
+        cursor.execute('select hub_id, cell_id, temperature, relay, hub_time, relayed_time'
+                       ' from temperatures order by hub_time desc limit 100')
+        return flask.render_template('temperatures.html', temperatures=cursor.fetchall())
 
     @staticmethod
     def post():
@@ -138,22 +116,15 @@ class Readings(flask.views.MethodView):
         d['relay'] = True  # TODO this should correspond to whether hub is in live mode
         cursor = db.cursor()
 
-        cursor.execute('select count(*) from readings where'
-                       ' cell_id=%(cell)s and hub_time=%(time)s and temperature=%(temp)s', d)
+        cursor.execute('select count(*) from temperatures where'
+                       ' cell_id=%(cell)s and temperature=%(temp)s and hub_time=%(time)s', d)
         count, = cursor.fetchone()
         if count:  # duplicate reading, store but don't relay
-            d['relay'] = False
+            d['relay'] = False  # TODO fix hub transmitter instead
 
-        cursor.execute('insert into readings (hub_id, hub_time, cell_id, temperature, relay)'
-                       ' values (%(hub)s, %(time)s, %(cell)s, %(temp)s, %(relay)s)', d)
+        cursor.execute('insert into temperatures (hub_id, cell_id, temperature, relay, hub_time)'
+                       ' values (%(hub)s, %(cell)s, %(temp)s, %(relay)s, %(time)s)', d)
         return 'ok'
-
-
-def get_hub_id(xbee_id):
-    cursor = db.cursor()
-    cursor.execute('select id from hubs where xbee_id=%s', (xbee_id,))
-    row = cursor.fetchone()
-    if row: return row['id']
 
 
 if __name__ == '__main__':
--- schema.sql
+++ schema.sql
@@ -1,30 +1,21 @@
-create table readings (
+create table temperatures (
     id serial primary key,
     hub_id text not null,
-    hub_time timestamp not null,
-    cell_id text not null
+    cell_id text not null,
     temperature real not null,
     relay boolean not null,
+    hub_time timestamp not null,
     time timestamp not null default now(),
     relayed_time timestamp
 );
-create index on readings (hub_id, time desc);
-create index on readings (cell_id, time desc);
+create index on temperatures (hub_id, time desc);
+create index on temperatures (cell_id, time desc);
 
 create table hubs (
-    id text primary key,
-    xbee_id text,
-    port integer,
-    time timestamp not null default now()
-);
-create index on hubs (xbee_id);
-
-create table hubs_log (
     id serial primary key,
     hub_id text not null,
-    xbee_id text,
+    pi_id text not null,
     port integer,
     time timestamp not null default now()
 );
-create index on hubs_log (hub_id, time desc);
-create index on hubs_log (xbee_id, time desc);
+create index on hubs (hub_id, time desc);
