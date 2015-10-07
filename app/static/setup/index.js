100d85d06fbde26d4b8f65ac6209d92b1e2a11e3
Harold Cooper <hrldcpr@gmail.com>
2015-10-07 16:50:00 -0400

    if the user types an o interpret it as a zero

--- app/static/setup/index.js
+++ app/static/setup/index.js
@@ -1,5 +1,5 @@
 $(function() {
     $('#go').click(function() {
-	location = '/' + $('#xbee_id').val().toLowerCase();
+	location = '/' + $('#xbee_id').val().toLowerCase().replace('o', '0');
     });
 });
