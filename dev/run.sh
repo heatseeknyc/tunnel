50179540ecfb9b774644ef8af21d3b4f8099ba48
Harold Cooper <hrldcpr@gmail.com>
2015-10-21 12:09:05 -0400

    dev db container with sample data

--- dev/run.sh
+++ dev/run.sh
@@ -10,10 +10,10 @@ docker rm db
 set -e
 
 docker build -t db db
-docker run --name db --volumes-from db-data -d db
+docker build -t db:dev dev/db
+docker run --name db --volumes-from db-data -d db:dev
 
 docker build -t app app
 docker build -t app:dev dev/app
-
 echo -e "\n###\n### Starting server at http://$(docker-machine ip $machine):$port\n###\n"
 docker run --name app --link db -v "$PWD/app":/opt/app:ro -p $port:$port app:dev
