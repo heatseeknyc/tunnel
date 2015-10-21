344d01999f876d3cd7bcdd0cf24c78dae1f57454
Harold Cooper <hrldcpr@gmail.com>
2015-10-21 12:10:15 -0400

    wait for db to start before starting app

--- dev/run.sh
+++ dev/run.sh
@@ -15,5 +15,11 @@ docker run --name db --volumes-from db-data -d db:dev
 
 docker build -t app app
 docker build -t app:dev dev/app
+
+while ! bash db/connect.sh psql -c select; do
+    echo 'waiting for db...'
+    sleep 1
+done
+
 echo -e "\n###\n### Starting server at http://$(docker-machine ip $machine):$port\n###\n"
 docker run --name app --link db -v "$PWD/app":/opt/app:ro -p $port:$port app:dev
