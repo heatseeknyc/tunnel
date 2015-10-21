9b430ba98d681384c6d48b1a1f9784c39b716fc6
Harold Cooper <hrldcpr@gmail.com>
2015-10-20 20:14:43 -0400

    print less junk when running dev/db.sh

--- db/connect.sh
+++ db/connect.sh
@@ -8,4 +8,6 @@ if [ -t 0 ]; then  # stdin is a terminal
     terminal="-t"
 fi
 
+set -x
+
 docker run --link db --rm -i $terminal postgres:9 sh -c "exec $command -h \"\$DB_PORT_5432_TCP_ADDR\" -p \"\$DB_PORT_5432_TCP_PORT\" -U postgres"
--- dev/db.sh
+++ dev/db.sh
@@ -1,2 +1,3 @@
 . dev/docker-machine.sh </dev/null 1>&2
+set +x
 . db/connect.sh $@
