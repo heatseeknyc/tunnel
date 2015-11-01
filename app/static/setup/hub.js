7417fbc3d466125a8653d941d913b6bafdf07b50
Harold Cooper <hrldcpr@gmail.com>
2015-11-01 17:55:30 -0500

    refresh data on hub setup page every 5 seconds

--- app/static/setup/hub.js
+++ app/static/setup/hub.js
@@ -1,7 +1,25 @@
 $(function() {
 
+  function refresh() {
+    $.when($.get(HUB_PARTIAL_URL), $.get(CELLS_PARTIAL_URL))
+      .done(function(hub_partial, cells_partial) {
+        console.log(hub_partial);
+        console.log(cells_partial);
+
+        $('#hub-data').html(hub_partial[0]);
+        $('#cells-data').html(cells_partial[0]);
+        refreshSoon();
+      });
+  }
+
+  function refreshSoon() {
+    setTimeout(refresh, 5000);
+  }
+
   $('#hourly').click(function() {
     $.ajax(HUB_URL, {method: 'PATCH', data: {hourly: true}});
   });
+
+  refreshSoon();
 
 });
